b3531541501d2b33218f1b37680f87f260e5d9f5
Xingan Wang <wangxgwxg@gmail.com>
2017-10-02 14:51:43 -0700

    fix credentials

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ app.use(async (ctx, next) => {
   const origin = ctx.get('Origin');
   if (ctx.method !== 'OPTIONS') {
     ctx.set('Access-Control-Allow-Origin', origin);
+    ctx.set('Access-Control-Allow-Credentials', 'true');
   } else if (ctx.get('Access-Control-Request-Method')) {
     ctx.set('Access-Control-Allow-Origin', origin);
     ctx.set('Access-Control-Allow-Methods', 'GET');
     ctx.set('Access-Control-Allow-Headers', 'Content-Type');
     ctx.set('Access-Control-Max-Age', '42');
+    ctx.set('Access-Control-Allow-Credentials', 'true');
   }
   await next();
 });
