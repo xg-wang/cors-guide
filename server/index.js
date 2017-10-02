c8ed7d7a1f3e67ae34f6581cbc8952f9662c8ce7
Xingan Wang <wangxgwxg@gmail.com>
2017-10-02 14:33:08 -0700

    fix preflight

--- server/index.js
+++ server/index.js
@@ -3,7 +3,15 @@ const app = new Koa();
 
 // cors middleware
 app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', 'http://localhost:8000');
+  const origin = ctx.get('Origin');
+  if (ctx.method !== 'OPTIONS') {
+    ctx.set('Access-Control-Allow-Origin', origin);
+  } else if (ctx.get('Access-Control-Request-Method')) {
+    ctx.set('Access-Control-Allow-Origin', origin);
+    ctx.set('Access-Control-Allow-Methods', 'GET');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type');
+    ctx.set('Access-Control-Max-Age', '42');
+  }
   await next();
 });
 
